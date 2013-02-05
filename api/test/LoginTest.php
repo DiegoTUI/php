12a10ec5944fe99435591e8bcad48f25dd5fe55e
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-05 13:40:42 +0100

    mock test added

---
+++ api/test/LoginTest.php
@@ -0,0 +1,68 @@
+<?php
+
+//include_once(__DIR__ . '/../../common/frameworkInclude.php');
+
+class LoginTest extends PHPUnit_Framework_TestCase
+{
+	protected $_common;
+
+	/**
+	 * @return void
+	 */
+	protected function setUp()
+	{
+		//$this->_common = new Test_Util_Common();
+		ob_start();
+	}
+
+	/**
+	 * Test login with a valid user.
+	 * @return void
+	 */
+	public function testLoginValidUser()
+	{
+		/*//login with "pechodelata" user
+		$_REQUEST["email"] = "pechodelata@kimia.es";
+		$_REQUEST["password"] = "pechodelata";
+		
+		//perform a valid login
+		$token = $this->_common->validLogin();*/
+
+		//clean the buffer for next test
+		ob_clean();
+	}
+
+	/**
+	 * Test login with an invalid email.
+	 * @return void
+	 */
+	/*public function testLoginInValidEmail()
+	{
+		//login with wrong "pechodelata" user
+		$_REQUEST["email"] = "pechodelata@kimmmia.es";
+		$_REQUEST["password"] = "pechodelata";
+		
+		$this->_common->invalidLogin();
+	}*/
+
+	/**
+	 * Test login with an invalid password.
+	 * @return void
+	 */
+	/*public function testLoginInValidPassword()
+	{
+		//login with wrong "pechodelata" user
+		$_REQUEST["email"] = "pechodelata@kimia.es";
+		$_REQUEST["password"] = "pechodelatas";
+
+		$this->_common->invalidLogin();
+	}*/
+
+	/**
+	 * @return void
+	 */
+	protected function tearDown()
+	{
+		ob_end_clean();
+	}
+}
