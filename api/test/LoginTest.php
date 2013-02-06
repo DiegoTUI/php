2f05dd8b42ef33221cc02d1b3f2866d10e55f7df
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-06 09:57:00 +0100

    trying to pass first test

--- api/test/LoginTest.php
+++ api/test/LoginTest.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once '../controller/Token.php';
-include_once './util/Commons.php';
+include_once ('../controller/Token.php');
+include_once ('./util/Commons.php');
 
 class LoginTest extends PHPUnit_Framework_TestCase
 {
