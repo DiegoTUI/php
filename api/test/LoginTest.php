5bac2044144bb5adf5a2210a02e74e5bdaedb346
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-06 15:56:10 +0100

    fixing first test

--- api/test/LoginTest.php
+++ api/test/LoginTest.php
@@ -1,7 +1,7 @@
 <?php
 //require_once 'PHPUnit/Autoload.php';
 include_once 'controller/Token.php';
-require_once 'test/util/KimiaCommon.php';
+require_once 'test/util/TestCommons.php';
 
 class LoginTest extends PHPUnit_Framework_TestCase
 {
