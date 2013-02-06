b3de8a1507345417249d4b8c1a1d8face474ee3c
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-06 13:07:59 +0100

    trying to pass first test

--- api/test/LoginTest.php
+++ api/test/LoginTest.php
@@ -2,7 +2,7 @@
 
 //include_once '../controller/Token.php';
 //include_once './util/Commons.php';
-require_once 'PHPUnit/Autoloader.php';
+require_once 'PHPUnit/Autoload.php';
 
 class LoginTest extends PHPUnit_Framework_TestCase
 {
