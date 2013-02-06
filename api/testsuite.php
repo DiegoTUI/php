b8e8bdc24fe2a3f89ea303666c750098fdeadd90
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-06 13:33:49 +0100

    trying to pass first test

--- api/testsuite.php
+++ api/testsuite.php
@@ -4,7 +4,7 @@ require_once 'PHPUnit/Autoload.php';
 require_once 'test/LoginTest.php';
 require_once 'PHPUnit.php';
 
-$suite  = new PHPUnit_TestSuite("TestSuite");
+$suite  = new PHPUnit_TestSuite("LoginTest");
 $phpunit = new PHPUnit();
 $result = $phpunit->run($suite);
 
