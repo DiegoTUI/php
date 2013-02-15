cac94dbadee97422f49b69a904af46b81abd9921
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-15 11:58:13 +0100

    included test in CONFIG global variable

--- api/util/UtilConfig.php
+++ api/util/UtilConfig.php
@@ -12,5 +12,5 @@ const VERSION = '0.0.9';
 $CONFIG = array(
 	// Used to know if we are testing or not
 	// Tests will set this to true on setUp and to false again in TearDown
-	'test' => false,
+	'test' => true,
 );
