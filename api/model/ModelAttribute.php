cf764c04651cc46251ba33e20fe47843b4d574a3
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-15 15:09:43 +0100

    starting with TicketAvailRQ

--- api/model/ModelAttribute.php
+++ api/model/ModelAttribute.php
@@ -18,9 +18,11 @@
  */
 class Attribute
 {
+	var $id;
 	var $name;
 	var $type;
 	var $path;
+	var $value;
 	var $mandatory = false;
 	var $writeable = true;
 	var $visible = true;
@@ -31,14 +33,17 @@ class Attribute
 	 */
 	function __construct($name, $type = "element", $dot_path = null)
 	{
+		$this->id = $name;
 		$this->name = $name;
 		$this->path = array($name);
 		if ($dot_path != null)
 		{
+			$this->id = str_replace(".", "_", $dot_path) . '_' . $name;
 			$this->set_path($dot_path);
 		}
 		
 		$this->type = $type;
+		$this->value = null;
 	}
 
 	/**
@@ -53,144 +58,14 @@ class Attribute
 	/**
 	 * Read a value from the request; if present, set into the given object.
 	 */
-	function read_set(&$object)
+	function read_set()
 	{
 		global $REQUEST;
 		if (!$this->writeable)
 		{
 			page_error('Read only attribute ' . $this->name);
 		}
-		$value = $REQUEST->read($this->name, false);
-		$this->set($object, $value);
-	}
-
-	/**
-	 * 
-	 * Flatten the value from an object and set into the flattened object.
-	 * The value is set as its name, so as to avoid nested attributes.
-	 */
-	function flatten($object, &$flattened)
-	{
-		if (!$this->visible)
-		{
-			return;
-		}
-		$value = $this->get($object);
-		if ($value === null)
-		{
-			return;
-		}
-		$flattened[$this->name] = $value;
-	}
-
-	/**
-	 * Filter the value from an object and set into the filtered object.
-	 */
-	function filter($object, &$filtered)
-	{
-		if (!$this->visible)
-		{
-			return;
-		}
-		$value = $this->get_inner($object);
-		if ($value === null)
-		{
-			return;
-		}
-		$this->set_inner($filtered, $value);
-	}
-
-	/**
-	 * Get the value of an attribute from an object.
-	 */
-	function get($object)
-	{
-		$value = $this->get_inner($object);
-		
-		return $value);
-	}
-
-	/**
-	 * Set the value for an attribute into an object.
-	 */
-	function set(&$object, $value)
-	{
-		if ($value == null && $this->mandatory)
-		{
-			page_error('Parameter "' . $this->name . '" is mandatory');
-		}
-		$this->set_inner($object, $value);
-	}
-
-	/**
-	 * Get the inner value of an object, untranslated.
-	 */
-	function get_inner($object)
-	{
-		return $this->inner($this->path, $object, function($inner, $attr_name)
-		{
-			if (!isset($inner[$attr_name]))
-			{
-				return null;
-			}
-			return $inner[$attr_name];
-		});
-	}
-
-	/**
-	 * Set the value into an inner attribute, unconverted.
-	 */
-	function set_inner(&$object, $value)
-	{
-		$this->inner($this->path, $object, function(&$inner, $attr_name) use ($value)
-		{
-			$inner[$attr_name] = $value;
-		});
-	}
-
-	/**
-	 * Locate the place referred to by the path (as an array of fragments)
-	 * and perform the given operation.
-	 */
-	function inner($path, &$object, $operation)
-	{
-		$piece = array_shift($path);
-		if (count($path) == 0)
-		{
-			return $operation($object, $piece);
-		}
-		$pos = null;
-		if (contains($piece, '[') && contains($piece, ']'))
-		{
-			$bits = explode('[', $piece);
-			$piece = $bits[0];
-			$pos = intval($bits[1]);
-		}
-		if (!isset($object[$piece]))
-		{
-			$object[$piece] = array();
-		}
-		if ($pos === null)
-		{
-			if (!is_array($object[$piece]))
-			{
-				return null;
-			}
-			return $this->inner($path, $object[$piece], $operation);
-		}
-		return $this->inner_array($path, $object[$piece], $pos, $operation);
-	}
-
-	/**
-	 * Perform the given operation given that $linear is truly a linear array.
-	 */
-	function inner_array($path, &$linear, $pos, $operation)
-	{
-		while (count($linear) < $pos + 1)
-		{
-			array_push($linear, array());
-		}
-		return $this->inner($path, $linear[$pos], $operation);
+		$this->value = $REQUEST->read($this->id, $this->mandatory);
 	}
 }
 
--- api/model/ModelEntity.php
+++ api/model/ModelEntity.php
@@ -30,264 +30,48 @@ class ModelEntity
 	{
 		foreach ($attributes as $attribute)
 		{
-			$this->attributes[$attribute->name] = $attribute;
+			$this->attributes[$attribute->id] = $attribute;
 		}
 	}
-
+	
 	/**
-	 * Transform an object, return a copy with all attributes in the first level.
-	 * Suitable for showing or editing.
+	 * Fill all the attributes with the contents of $REQUEST
 	 */
-	function flatten($object)
-	{
-		$copy = array();
-		foreach ($this->attribute as $attribute)
+	 function read_set_all()
+	 {
+		foreach($this->attributes as $attribute)
 		{
-			$attribute->flatten($object, $copy);
+			$attribute->read_set()
 		}
-		return $copy;
-	}
+	 }
+	
 
 	/**
-	 * Filter an object, return a copy with all non-visible objects removed.
+	 * Get the attribute with the given id.
 	 */
-	function filter($object)
+	function get_attribute($id)
 	{
-		$copy = array();
-		foreach ($this->attributes as $attribute)
+		if (!isset($this->attributes[$id]))
 		{
-			$attribute->filter($object, $copy);
-		}
-		return $copy;
-	}
-
-	/**
-	 * Get the attribute with the given key.
-	 */
-	function get_attribute($key)
-	{
-		if (!isset($this->attributes[$key]))
-		{
-			page_error('Unknown attribute ' . $key . ' for ' . $this->name);
+			page_error('Unknown attribute ' . $id . ' for ' . $this->name);
 		}
 		return $this->attributes[$key];
 	}
 
 	/**
-	 * Get the value of an attribute from an object by key.
+	 * Get the value of an attribute using its id.
 	 */
-	function get_value($object, $key)
+	function get_value($id)
 	{
-		return $this->get_attribute($key)->get($object);
+		return $this->get_attribute($id)->$value;
 	}
 
 	/**
-	 * Set the value for an attribute into an object.
+	 * Set the value for an attribute using its id.
 	 */
-	function set_value(&$object, $key, $value)
+	function set_value($id, $value)
 	{
-		$this->get_attribute($key)->set($object, $value);
-	}
-
-	/**
-	 * Create a new object with a random id, read contents from the request.
-	 * Returns the new object.
-	 */
-	function create_new()
-	{
-		global $AUTHORIZATION;
-		$id = $this->read_id(false);
-		if (isset($id))
-		{
-			page_error('New ' . $this->name . ' cannot accept external primary key');
-		}
-		$object = $this->get_skeleton();
-		foreach ($this->attributes as $key => $attribute)
-		{
-			if ($attribute->mandatory)
-			{
-				$attribute->read_set($object);
-			}
-		}
-		$object['created'] = $AUTHORIZATION->get_audit_info();
-		return $object;
-	}
-
-	/**
-	 * Modify an existing object with all values read from the request.
-	 */
-	function modify_existing(&$object)
-	{
-		global $REQUEST;
-		foreach($REQUEST->get_keys() as $key)
-		{
-			$this->get_attribute($key)->read_set($object);
-		}
-	}
-
-	/**
-	 * Get a list attribute with the given key. Shows an error if not a list.
-	 */
-	function get_list_attribute($key)
-	{
-		$list_attribute = $this->get_attribute($key);
-		if (!($list_attribute instanceof BasicList))
-		{
-			page_error('Attribute ' . $key . ' is not a list');
-		}
-		return $list_attribute;
-	}
-	/**
-	 * Check that a value is in a list attribute of an object.
-	 * Returns true if the value is in the list.
-	 * If the attribute is not a list shows an error.
-	 */
-	function check_in_list($object, $key, $value)
-	{
-		$list_attribute = $this->get_list_attribute($key);
-		return $list_attribute->check($value, $object);
-	}
-
-	/**
-	 * Add a value to a list attribute in an object.
-	 */
-	function add_in_list(&$object, $key, $value)
-	{
-		$list_attribute = $this->get_list_attribute($key);
-		return $list_attribute->add($value, $object);
-	}
-
-	/**
-	 * Remove a value from a list attribute in an object.
-	 */
-	function remove_in_list(&$object, $key, $value)
-	{
-		$list_attribute = $this->get_list_attribute($key);
-		return $list_attribute->remove($value, $object);
-	}
-
-	/**
-	 * Filter an element in a list using the entity definition for list elements.
-	 * Returns a filtered copy of the element.
-	 */
-	function filter_in_list($key, $value)
-	{
-		$list_attribute = $this->get_list_attribute($key);
-		return $list_attribute->filter_element($value);
-	}
-
-	/**
-	 * Get the primary key from the parameter or from the request, look for it in a list attribute.
-	 * Returns the value from the list with the primary key, or null if not found.
-	 */
-	function find_in_list($object, $key, $id = null)
-	{
-		$list_attribute = $this->get_list_attribute($key);
-		if ($id == null)
-		{
-			$id = $list_attribute->read_id();
-		}
-		return $list_attribute->find_id($id, $object);
-	}
-
-	/**
-	 * Get an object with just the primary key of a list element.
-	 */
-	function get_skeleton_in_list($key, $value)
-	{
-		$list_attribute = $this->get_list_attribute($key);
-		return $list_attribute->entity->get_skeleton($value);
-	}
-
-	/**
-	 * Set the content as JSON.
-	 */
-	function set_json_content()
-	{
-	    //header('Content-type: application/json');
-		header('Content-type: text/plain');
-	}
-
-	/**
-	 * Show an object as JSON, do not exit afterwards.
-	 */
-	function show($object)
-	{
-		$result = $this->flatten($object);
-		$this->show_json($result);
-	}
-
-	/**
-	 * Show all elements in an object (or a cursor) as JSON, do not exit afterwards.
-	 * A cursor is especially marked (true by default).
-	 * The resulting JSON object will show total number of elements,
-	 * count of elements returned, and an array with the values themselves.
-	 */
-	function show_all($object, $cursor = true)
-	{
-		if ($object === null)
-		{
-			page_error('Empty array');
-		}
-		$values = array();
-		foreach ($object as $attribute)
-		{
-			$values[] = $this->flatten($attribute);
-		}
-		if ($cursor)
-		{
-			$total = $object->count();
-			$count = count($values);
-			$info = $object->info();
-			$offset = $info['skip'];
-		}
-		else
-		{
-			$total = count($values);
-			$count = count($values);
-			$offset = 0;
-		}
-		$this->show_values($total, $count, $offset, $values);
-	}
-
-	/**
-	 * Show a number of values, including: total, count, offset and values.
-	 */
-	function show_values($total, $count, $offset, $values)
-	{
-		$result = array(
-			'total' => $total,
-			'count' => $count,
-			'offset' => $offset,
-			'values' => $values,
-		);
-		$this->show_json($result);
-	}
-
-	/**
-	 * Show an object as JSON.
-	 */
-	function show_json($result)
-	{
-		$this->set_json_content();
-
-		$now = date(DATE_RFC822);
-		header('Cache-Control: public, no-store, max-age=0');
-		header('Expires: '.$now);
-		header('Last-Modified: '.$now);
-		header('Vary: *');
-
-		print json_encode($result);
-		monitor('OK', $result);
-	}
-
-	/**
-	 * Show a list in an object.
-	 */
-	function show_list($object, $key)
-	{
-		$list_attribute = $this->get_list_attribute($key);
-		$list_attribute->show($object);
+		$this->get_attribute($key)->value = $value;
 	}
 }
 
