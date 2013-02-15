8cb9abc716121510f19051fd4360bba46fc38e89
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-15 12:10:47 +0100

    included test in CONFIG global variable

--- api/error/error.php
+++ api/error/error.php
@@ -10,6 +10,8 @@ function send_page($header, $page, $message = null)
 {
 	global $CONFIG;
 	global $ERROR_MESSAGE;
+	UtilLogging::getInstance()->debug("Value of true: " . true);
+	UtilLogging::getInstance()->debug("Value of false: " . false);
 	UtilLogging::getInstance()->debug("Value of CONFIG[test]: " . $CONFIG["test"]);
 	if ($CONFIG["test"])
 		throw new TuiException ("page error");
