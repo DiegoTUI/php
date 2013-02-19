6c388e7dcc0c3414871e5af9547eab175612df45
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-19 18:44:01 +0100

    get_xml working

--- api/model/ModelEntity.php
+++ api/model/ModelEntity.php
@@ -98,13 +98,13 @@ class ModelEntity
 		//attributes
 		if (isset($body["attribute"]))
 		{
-			if (is_array($body["attribute"]))
-			{
+			//if (is_array($body["attribute"]))
+			//{
 				foreach ($body["attribute"] as $key=>$value)
 				{
 					$result = $result . " " . $key . "=\"" . $value . "\"";
 				}
-			}
+			//}
 			unset($body["attribute"]);
 		}
 		$result = $result . ">\n";
