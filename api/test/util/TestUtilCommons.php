3ca52eaf3754c5f5305f9b265539ece2c7f20a24
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-21 13:59:39 +0100

    refining ticketAvail tests

--- api/test/util/TestUtilCommons.php
+++ api/test/util/TestUtilCommons.php
@@ -276,7 +276,7 @@ class TestUtilCommons extends PHPUnit_Framework_TestCase
 			if (equals($name, "Value"))
 			{
 				TestUtilLogging::getInstance()->debug("Attribute name: " . $name . " and value: " . $attribute->value);	
-				var_dump ($cursor);
+				var_dump ($simpleXML->Destination);
 				$stringOutput = ob_get_clean();
 				TestUtilLogging::getInstance()->debug('cursor: ' . $stringOutput);
 				$this->assertEquals($attribute->value, $cursor[0]);
