0aa69c0888d6494aabd81c9befaaf364acb871db
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-21 13:31:28 +0100

    refining ticketAvail tests

--- api/test/util/TestUtilCommons.php
+++ api/test/util/TestUtilCommons.php
@@ -267,9 +267,9 @@ class TestUtilCommons extends PHPUnit_Framework_TestCase
 		}
 		//check that the value is correct
 		if(equals($attribute->type, "attribute"))
-			this->assertEquals($attribute->value, cursor[$attribute->name]);
+			$this->assertEquals($attribute->value, cursor[$attribute->name]);
 		else if(equals($attribute->type, "element"))
-			this->assertEquals($attribute->value, cursor->($attribute->name));
+			$this->assertEquals($attribute->value, cursor->($attribute->name));
 	 }
 	
 	/**
