b476b97de7574198ea06d6ae39050edfd2ac4883
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-22 14:46:02 +0100

    trying some more stuff with headers

--- api/test/TicketAvailRQTest.php
+++ api/test/TicketAvailRQTest.php
@@ -135,7 +135,8 @@ class TicketAvailRQTest extends PHPUnit_Framework_TestCase
 		$post = array($CONFIG['parameter_name'] => $xml);
 		$post_body = json_encode($post);
 		$this->_common->debug("POST_body :" . $post_body . "\n");
-		$request->setBody(json_encode($post));
+		//$request->setBody(json_encode($post));
+		$request->setPostFields($post);
 		$request->setHeaders(array("Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 									"Accept-Encoding" => "gzip, deflate",
 									"Content-Type" => "text/plain; charset=UTF-8"));
