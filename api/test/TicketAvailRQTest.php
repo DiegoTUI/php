f138a6dd4fcf2cc3e9feb02dd9fbf082f9376434
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-22 09:34:54 +0100

    dealing with headers

--- api/test/TicketAvailRQTest.php
+++ api/test/TicketAvailRQTest.php
@@ -138,6 +138,8 @@ class TicketAvailRQTest extends PHPUnit_Framework_TestCase
 		$request->setBody(json_encode($post));
 		$request->setHeaders(array("Host" => "212.170.239.71",
 									"X-Real-IP" => "54.246.80.107",
+									"X-Forwarded-For" => "54.246.80.107",
+									"Connection" => "close",
 									"User-Agent" => "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0",
 									"Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 									"Accept-Language" => "en-US,en;q=0.5",
