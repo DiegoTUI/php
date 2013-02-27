78e61014dac4ea9130bf0250fde984a8980841ad
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-02-27 13:50:49 +0100

    removing useless lines

--- api/test/TicketAvailRQTest.php
+++ api/test/TicketAvailRQTest.php
@@ -133,19 +133,11 @@ class TicketAvailRQTest extends PHPUnit_Framework_TestCase
 		global $CONFIG;
 		$url = $CONFIG['url_test_http'];
 		$request = new HTTPRequest($url, HTTP_METH_POST);
-		//$post = array($CONFIG['parameter_name'] => $xml);
-		//$post_body = json_encode($post);
 		$this->_common->debug("POST_body :" . $xml . "\n");
-		//$request->setBody($post_body);
 		$request->setBody($xml);
-		/*$request->setPostFields($post);*/
 		$request->setHeaders(array("Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,;q=0.8",
 									"Accept-Encoding" => "gzip, deflate",
 									"Content-Type" => "text/plain; charset=UTF-8"));
-		/*var_dump($request->getHeaders());
-		$stringOutput = ob_get_clean();
-		$this->_common->debug("Headers :" . $stringOutput . "\n");
-		$this->_common->debug("Read POST fields: " . prettyPrintJSON(json_encode($request->getPostFields())));*/
 		$request->send();
 		$response = $request->getResponseBody();
 		$this->_common->debug("ATLAS response :" . $response . "\n");
--- api/util/UtilConfig.php
+++ api/util/UtilConfig.php
@@ -24,27 +24,7 @@ $CONFIG = array(
 	
 	//User and password authorized to make the calls
 	'user' => 'BDS',
-	'password' => 'BDS',
-	
-	/*name of the parameter in the POST call
-	'parameter_name' => 'xml_request',
-	
-	//xml headers and footers
-	'xml_headers' => array(
-		'TicketAvailRQ' => "<soapenv:Envelope" . "\n" . 
-			'soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"' ."\n" . 
-			'xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' . "\n" .
-			'<soapenv:Body>' . "\n" . 
-			'<hb:getTicketAvail xmlns:hb="http://axis.frontend.hydra.hotelbeds.com" xsi:type="xsd:string">' . "\n",
-			
-		'TicketAvailRQ_noSOAP' => '<hb:getTicketAvail xmlns:hb="http://axis.frontend.hydra.hotelbeds.com" xsi:type="xsd:string">' . "\n"),
-							
-	'xml_footers' => array (
-		'TicketAvailRQ' => '</hb:getTicketAvail>' . "\n" . 
-			'</soapenv:Body>' . "\n" . 
-			'</soapenv:Envelope>',
-			
-		'TicketAvailRQ_noSOAP' => '</hb:getTicketAvail>')*/
+	'password' => 'BDS'
 );
 
 date_default_timezone_set('UTC');
