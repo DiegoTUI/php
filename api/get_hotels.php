76fc73cf80209d3de2e4f4b9d1a2a5d571b9cb5f
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-03-12 10:44:18 +0100

    added get_hotels

--- api/get_hotels.php
+++ api/get_hotels.php
@@ -5,7 +5,7 @@ include_once './util/UtilMongo.php';
 $query = array ("location.code" => "PMI",
 				"category" => "5EST");
 $cursor = UtilMongo::getInstance()->getCollection("hotels")->find($query);
-return = array();
+$return = array();
 
 while ($cursor->hasNext())
 {
