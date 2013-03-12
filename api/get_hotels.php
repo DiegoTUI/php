ba3d1342cfa62fc02f64ec9a6135538e1f6275dc
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-03-12 13:21:23 +0100

    latitude and longitude not null

--- api/get_hotels.php
+++ api/get_hotels.php
@@ -3,7 +3,9 @@
 include_once './util/UtilMongo.php';
 
 $query = array ("location.code" => "PMI",
-				"category" => "5EST");
+				"category" => "5EST",
+				"location.latitude" => array('$ne'=>null),
+				"location.longitude" => array('$ne'=>null));
 $cursor = UtilMongo::getInstance()->getCollection("hotels")->find($query);
 $return = array();
 
