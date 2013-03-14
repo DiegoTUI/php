f960a67cd6a76a2ee9b5bb0990d82991f5757aa5
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-03-14 16:22:38 +0100

    get_hotels now perform real geolocated search

--- api/get_hotels.php
+++ api/get_hotels.php
@@ -2,17 +2,25 @@
 
 include_once './util/UtilMongo.php';
 
-$query = array ("location.code" => "PMI",
-				"category" => "5EST",
-				"location.latitude" => array('$ne'=>null),
-				"location.longitude" => array('$ne'=>null));
+$params = $_REQUEST["params"];
+
+if (!$params) return json_encode(array("message"=>"ERROR: no params provided"));
+if (!array_key_exists("latitude", $params)) return json_encode(array(message=>"ERROR: no latitude provided"));
+if (!array_key_exists("longitude", $params)) return json_encode(array(message=>"ERROR: no longitude provided"));
+
+$longitude = (double)($params["longitude"]);
+$latitude = (double)($params["latitude"]);
+$londelta = array_key_exists("londelta", $params) ? (double)($params["londelta"]) : 0.1;
+$latdelta = array_key_exists("latdelta", $params) ? (double)($params["latdelta"]) : 0.1;
+
+$query = array ("loc" => array("$within" => array("$box" => array(array($longitude - $londelta, $latitude - $latdelta), array($longitude + $londelta, $latitude + $latdelta)))));
 $cursor = UtilMongo::getInstance()->getCollection("hotels")->find($query);
-$return = array();
+$return = array("message" => "OK", "response" => array());
 
 while ($cursor->hasNext())
 {
 	$result = $cursor->getNext();
-	array_push($return, $result);
+	array_push($return["response"], $result);
 }
 
 echo json_encode($return);
