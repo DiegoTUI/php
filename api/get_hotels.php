a2d565d121c2a345098be7e9c46bd9bb1bd9c802
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-03-14 16:36:18 +0100

    get_hotels now perform real geolocated search

--- api/get_hotels.php
+++ api/get_hotels.php
@@ -3,10 +3,10 @@
 include_once './util/UtilMongo.php';
 
 $params = $_REQUEST["params"];
-var_dump($params);
+
 if (!$params) return json_encode(array("message"=>"ERROR: no params provided"));
 $params = json_decode($params, true);
-var_dump($params);
+
 if (!array_key_exists("latitude", $params)) return json_encode(array(message=>"ERROR: no latitude provided"));
 if (!array_key_exists("longitude", $params)) return json_encode(array(message=>"ERROR: no longitude provided"));
 
@@ -16,6 +16,7 @@ $londelta = array_key_exists("londelta", $params) ? (double)($params["londelta"]
 $latdelta = array_key_exists("latdelta", $params) ? (double)($params["latdelta"]) : 0.1;
 
 $query = array ("loc" => array("$within" => array("$box" => array(array($longitude - $londelta, $latitude - $latdelta), array($longitude + $londelta, $latitude + $latdelta)))));
+echo "Number of hotels in the query: " . UtilMongo::getInstance()->getCollection("hotels")->count($query);
 $cursor = UtilMongo::getInstance()->getCollection("hotels")->find($query);
 $return = array("message" => "OK", "response" => array());
 
