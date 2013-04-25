c3087303996c22782e2dad6cd8b632f6df1d4ee2
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-04-25 13:07:20 +0200

    added test_post

--- api/test_post.php
+++ api/test_post.php
@@ -3,7 +3,7 @@
 $field1 = $_GET["field1"];
 $field2 = $_GET["field2"];
 
-$post_body = @file_get_contents(STDIN);
+$post_body = @file_get_contents('php://input');
 
 echo "Post received - field1: " . $field1 . " - field2: " . $field2 . " - post body: " . $post_body;
 
