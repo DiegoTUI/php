cd260918724b49c171812f768dae9a5e74898094
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-04-25 14:50:21 +0200

    test_post

--- api/test_post.php
+++ api/test_post.php
@@ -5,10 +5,6 @@ $field2 = $_POST["field2"];
 
 $post_body = @file_get_contents('php://input');
 
-$headers = apache_request_headers();
-
-var_dump($headers);
-
 echo "Post received - field1: " . $field1 . " - field2: " . $field2 . " - post body: " . $post_body;
 
 ?>
