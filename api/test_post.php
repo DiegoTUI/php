e55b144e4f10deb4eec0f052e9c4af5931de6c16
DiegoTUI <dlafuente@tuitravel-ad.com>
2013-05-22 11:07:58 +0200

    test_post updated

--- api/test_post.php
+++ api/test_post.php
@@ -5,6 +5,8 @@ $field2 = $_POST["field2"];
 
 $post_body = @file_get_contents('php://input');
 
-echo "Post received - field1: " . $field1 . " - field2: " . $field2 . " - post body: " . $post_body;
+$result = array("rf1" => $field1, "rf2" => $field2, "rfb" => $post_body);
+
+echo json_encode($result);
 
 ?>
